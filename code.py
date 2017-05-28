f677c1bceb2330ad83ed24090ec0b9429c8e1c05
Kunallfc <kb8495@gmail.com>
2017-05-28 22:47:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -19,32 +19,29 @@ test_username = input("Enter your snapdeal username: ")
 test_password = input("Enter you snapdeal password: ")
 test_search = input("Enter name of product you want to search: ")
 
-# Load webdriver 
-#driver = webdriver.PhantomJS()
 driver = webdriver.Chrome()
 
-# Access URL
+
 driver.get("https://www.snapdeal.com/login")
 wait = ui.WebDriverWait(driver, 10)
 wait.until(page_is_loaded)
 
-# Enter email name and check user
 email_field = driver.find_element_by_id("userName")
 email_field.send_keys(test_username)
 driver.find_element_by_id("checkUser").click()
 
-# Enter password and login
+
 wait = WebDriverWait(driver, 10)
 password_field = wait.until(EC.element_to_be_clickable((By.ID,'j_password_login_uc')))
 password_field.send_keys(test_password)
 driver.find_element_by_id("submitLoginUC").click()
 
-# Search product
+
 search_field = wait.until(EC.element_to_be_clickable((By.ID,'inputValEnter')))
 search_field.send_keys(test_search)
 driver.find_element_by_class_name("searchformButton").click()
 
-# Open first product page
+
 products = wait.until(EC.element_to_be_clickable((By.ID,'products')))
 product = products.find_element_by_class_name("product-tuple-listing")
 product.click()
